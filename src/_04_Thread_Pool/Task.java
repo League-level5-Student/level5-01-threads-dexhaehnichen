52e3c82569aab338abff6ee59b25cb77f72a6886
Dexter H <dexhaehnic@gmail.com>
2020-09-28 18:27:30 -0700

    Started work on threadPool

--- src/_04_Thread_Pool/Task.java
+++ src/_04_Thread_Pool/Task.java
@@ -2,4 +2,7 @@ package _04_Thread_Pool;
 
 public interface Task {
 	
+	static void perform() {
+		System.out.println();
+	}
 }
--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,35 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ThreadPool {
 
+	Thread[] threads;
+	ConcurrentLinkedQueue<Task> queue;
+	
+	ThreadPool(int totalThreads){
+		threads = new Thread[totalThreads];
+		
+		for (int i = 0; i < threads.length; i++) {
+			threads[i] = new Thread(new Worker(queue));
+		}
+		
+		queue = new ConcurrentLinkedQueue();
+	}
+	
+	public void addTask(Task task) {
+		queue.add(task);
+	}
+	
+	public void start() {
+		for (int i = 0; i < threads.length; i++) {
+			threads[i].start();
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
